63508627597c5960ba931d427c0951c4a4b20e81
vannessza <vanthony@student.ciputra.ac.id>
2022-10-24 20:48:45 +0700

    Finish

--- cirestapi/application/models/Mahasiswamodel.php
+++ cirestapi/application/models/Mahasiswamodel.php
@@ -1,15 +1,15 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-
-class Usermodel extends CI_Model{
-
-    public function __construct(){
+class Mahasiswamodel extends CI_Model
+{
+    public function __construct()
+    {
         parent::__construct();
     }
 
-    public function getUser(){
-        $result = $this->db->get('user');
-        return $result-> result_array();
+    public function getMahasiswa()
+    {
+        $result = $this->db->get('mahasiswa');
+        return $result->result_array();
     }
-
 }
